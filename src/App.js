e7993f4831ff9b3d2850b769089f0bd2386e6723
solis-enterprise <greenlight8004@gmail.com>
2019-09-07 16:17:35 -0400

    Alot of new stuff added (mod)

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: "red",
-      isLoggedIn: "true",
+      user: "",
+      isLoggedIn: "false",
       errorMessage: ""
     };
     this.checkLogin = this.checkLogin.bind(this);
